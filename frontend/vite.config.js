a8ec35b2b92b690ae2d5ce59784c183d698ff5bd
Juli Kumari <117572954+julikray@users.noreply.github.com>
2025-02-05 13:32:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server : { proxy: "https://realtime-chat-backend-q8xl.onrender.com" || "http://localhost:4000" },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
