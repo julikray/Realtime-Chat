f753fe6328e2fcb82fdaa023a3041c8c5f101750
Juli Kumari <117572954+julikray@users.noreply.github.com>
2025-02-05 14:16:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,19 @@ const app = express();
 const port = process.env.PORT;
 
 
+// app.use(cors({
+//   origin: process.env.ORIGIN || 'https://realtime-chat-frontend-iwf9.onrender.com', 
+//   credentials: true,
+// }));
+
 app.use(cors({
-  origin: process.env.ORIGIN || 'https://realtime-chat-frontend-iwf9.onrender.com', 
+  origin: ['http://localhost:5173/', 'https://realtime-chat-frontend-iwf9.onrender.com/'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
+  exposedHeaders: ['*', 'Authorization']
 }));
+
 
 
 app.use(express.json());
@@ -53,6 +62,11 @@ setupSocket(server);
 
 
 app.use((err, req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
