2e9840f8c2e06d9dfa25fe18c41e1bc424f35ae3
Juli <julik.roy@gmail.com>
2025-02-05 12:36:45 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT;
 
 
 app.use(cors({
-  origin: process.env.ORIGIN || 'https://realtime-chat-frontend-tviv.onrender.com', 
+  origin: process.env.ORIGIN || 'http://localhost:5173', 
   credentials: true,
 }));
 
