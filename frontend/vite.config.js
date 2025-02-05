5847122b463f93eb5c18247d11525ab7ee877cc7
Juli Kumari <117572954+julikray@users.noreply.github.com>
2025-02-05 16:28:14 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,38 +1,14 @@
-// import path from "path"
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//    server: {
-//     proxy: {
-//       '/api': 'https://realtime-chat-backend-q8xl.onrender.com'
-//     },
-//    },
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
-
-
-
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_NODE_ENV === 'development' 
-          ? 'http://localhost:5173/'
-          : 'https://realtime-chat-backend-q8xl.onrender.com/',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  plugins: [react()]
 })
+
